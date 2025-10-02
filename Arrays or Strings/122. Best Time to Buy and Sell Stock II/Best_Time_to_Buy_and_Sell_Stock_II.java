1b26e938da0cbe24d6a009acc959e7627ebc0004
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-10-02 10:12:32 +0530

    Create Best_Time_to_Buy_and_Sell_Stock_II.java

---
+++ Arrays or Strings/122. Best Time to Buy and Sell Stock II/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,18 @@
+class Solution {
+    // public static void main(String[] args)
+    // {
+    //     int prices[]={7,1,5,3,6,4};
+    //     System.out.println(maxProfit(prices));
+    // }
+    public int maxProfit(int[] prices) {
+        int max_profit=0;
+        for(int i=0;i<prices.length-1;i++)
+        {
+            if(prices[i]<prices[i+1])
+            {
+                max_profit=max_profit + (prices[i+1] - prices[i]);
+            }
+        }
+        return max_profit;
+    }
+}
