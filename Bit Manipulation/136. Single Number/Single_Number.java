4694171ab5bcccbc8be636fa6dd9bdfdfbc010c9
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-10-03 12:06:52 +0530

    Create Single_Number.java

---
+++ Bit Manipulation/136. Single Number/Single_Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    // public static void main(String[] args)
+    // {
+    //     int nums[]={2,2,1};
+    //     System.out.println(singleNumber(nums));
+    // }
+    public int singleNumber(int[] nums) {
+        int value=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            value=value ^ nums[i];
+        }
+        return value;
+    }
+}
