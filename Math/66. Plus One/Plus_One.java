af6178f88af21efcb3c41df0256d1f68d8b2349c
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-10-13 06:11:59 +0530

    Update Plus_One.java

--- Math/66. Plus One/Plus_One.java
+++ Math/66. Plus One/Plus_One.java
@@ -1,4 +1,9 @@
 class Solution {
+    // public static void main(String[] args)
+    // {
+    //     int[] digits={1,2,3};
+    //     System.out.println(Arrays.toString(plusOne(digits)));
+    // }
     public int[] plusOne(int[] digits) {
         int n=digits.length;
         for(int i=n-1;i>=0;i--)
